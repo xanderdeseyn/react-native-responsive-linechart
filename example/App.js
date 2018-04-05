15ef831eea31fbac5ec54c39fea0dcd70e8c73f3
Xander Deseyn <xander.deseyn@gmail.com>
2018-04-05 15:06:35 +0200

    Updated example to latest version

--- example/App.js
+++ example/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import LineChart from './LineChart';
+import LineChart from 'react-native-responsive-linechart';
 
 export default class App extends React.Component {
   render() {
