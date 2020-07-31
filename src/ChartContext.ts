3678956d59b39e593d13ac19724a39571752d62f
Xander Deseyn <xander@devizer.io>
2020-07-31 17:20:13 +0200

    fix context

--- src/ChartContext.ts
+++ src/ChartContext.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ChartContext as TChartContext } from './types'
 
-const ChartContext = React.createContext<TChartContext>({ data: [], dimensions: undefined })
+const ChartContext = React.createContext<TChartContext>({ data: [], dimensions: undefined, domain: { x: { min: 0, max: 0 }, y: { min: 0, max: 0 } } })
 
 export const ChartContextProvider = ChartContext.Provider
 
