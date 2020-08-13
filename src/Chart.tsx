198eac2fd6037e94e7137f34ffc009d5b3b2c4bc
Xander Deseyn <xander@devizer.io>
2020-08-13 14:40:55 +0200

    more comments

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -10,10 +10,15 @@ import { ChartContextProvider } from './ChartContext'
 import { calculateDataDimensions } from './Chart.utils'
 
 type Props = {
+  /* All styling can be used except for padding. If you need padding, use the explicit `padding` prop below.*/
   style?: ViewStyle
+  /* Data to be used by `<Area />` or `<Line />` children. Not required, and can be overridden in Area or Line components. */
   data?: ChartDataPoint[]
+  /* Domain for the horizontal (X) axis. */
   xDomain?: AxisDomain
+  /* Domain for the vertical (Y) axis. */
   yDomain?: AxisDomain
+  /* Padding of the chart. Use this instead of setting padding in the `style` prop. */
   padding?: Padding
 }
 
