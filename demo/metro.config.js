8bea132268c594ac6673d98e7b2c1d3734d45703
Xander Deseyn <xander@devizer.io>
2020-08-16 16:42:55 +0200

    Reset demo project to use local files

--- demo/metro.config.js
+++ demo/metro.config.js
@@ -5,29 +5,29 @@
  * @format
  */
 
-// const path = require('path')
+const path = require('path')
 
-// const extraNodeModules = {
-//   // 'react-native-responsive-linechart': path.resolve(__dirname + '/../src')
-// }
+const extraNodeModules = {
+  'react-native-responsive-linechart': path.resolve(__dirname + '/../src'),
+}
 
-// const watchFolders = [path.resolve(__dirname + '/../src')]
+const watchFolders = [path.resolve(__dirname + '/../src')]
 
-// module.exports = {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: true,
-//         inlineRequires: false,
-//       },
-//     }),
-//   },
-//   resolver: {
-//     extraNodeModules: new Proxy(extraNodeModules, {
-//       get: (target, name) =>
-//         //redirects dependencies referenced from common/ to local node_modules
-//         name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
-//     }),
-//   },
-//   watchFolders,
-// }
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common/ to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+  watchFolders,
+}
