7005e786cadddc7df85a9603442fb921aa10d59f
Xander Deseyn <xander@devizer.io>
2020-09-14 10:16:17 +0200

    Implement tooltip selection callback

--- src/Line.tsx
+++ src/Line.tsx
@@ -17,12 +17,15 @@ type Props = {
   tension?: number
   /** Component to render tooltips. An example component is included: <BoxTooltip />. */
   tooltipComponent?: JSX.Element
+  /** Callback method that fires when a tooltip is displayed for a data point. */
+  onTooltipSelect?: (value: ChartDataPoint, index: number) => void
   /** Data for the chart. Overrides optional data provided in `<Chart />`. */
   data?: ChartDataPoint[]
 }
 
 const Line: React.FC<Props> = (props) => {
   const { data: contextData, dimensions, domain, lastTouch } = React.useContext(ChartContext)
+  const [tooltipIndex, setTooltipIndex] = React.useState<number | undefined>(undefined)
 
   const {
     theme: { stroke },
@@ -30,14 +33,26 @@ const Line: React.FC<Props> = (props) => {
     data = contextData,
     tension,
     smoothing,
+    onTooltipSelect,
   } = deepmerge(defaultProps, props)
 
   if (!dimensions) {
     return null
   }
 
+  React.useEffect(() => {
+    const scaledPoints = scalePointsToDimensions(data, domain, dimensions)
+    const newIndex = calculateTooltipIndex(scaledPoints, lastTouch)
+
+    if (newIndex !== tooltipIndex) {
+      setTooltipIndex(newIndex)
+      if (typeof onTooltipSelect === 'function' && typeof newIndex === 'number' && data.length > newIndex) {
+        onTooltipSelect(data[newIndex], newIndex)
+      }
+    }
+  }, [data, domain, dimensions, lastTouch, stroke, tension, smoothing])
+
   const scaledPoints = scalePointsToDimensions(data, domain, dimensions)
-  const tooltipIndex = calculateTooltipIndex(scaledPoints, lastTouch)
   const adjustedPoints = adjustPointsForThickStroke(scaledPoints, stroke)
 
   const pointsWithinDimensions = adjustedPoints.filter((p) => p.x >= 0 && p.x <= dimensions.width)
