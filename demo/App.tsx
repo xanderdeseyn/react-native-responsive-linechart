49ae98ce0b72278432f6844bf87abee6687dbef6
Xander Deseyn <xander@devizer.io>
2020-09-16 16:02:19 +0200

    Better demo

--- demo/App.tsx
+++ demo/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
     <Container>
       <StatusBar barStyle="dark-content" />
       <Chart
-        style={{ height: 200, width: 400 }}
+        style={{ height: 200, width: '100%', marginTop: 40 }}
         padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
         xDomain={{ min: -2, max: 10 }}
         yDomain={{ min: 0, max: 20 }}
@@ -44,12 +44,12 @@ const App = () => {
         />
       </Chart>
       <Chart
-        style={{ height: 200, width: '100%', marginTop: 100, backgroundColor: '#f2f2f2' }}
+        style={{ height: 200, width: '100%' }}
         data={data2}
         padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
         xDomain={{ min: 0, max: 10 }}
         yDomain={{ min: 0, max: 20 }}
-        viewport={{ size: { width: 5, height: 10 } }}
+        viewport={{ size: { width: 5 } }}
       >
         <VerticalAxis
           tickValues={[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]}
@@ -72,13 +72,95 @@ const App = () => {
         <Line smoothing="cubic-spline" tension={0.3} theme={{ stroke: { color: 'green', width: 2 } }} />
         <Line tension={0.3} theme={{ stroke: { color: 'orange', width: 2 }, scatter: { default: { width: 4, height: 4 } } }} /> */}
       </Chart>
+      <Chart
+        style={{ height: 200, width: '100%' }}
+        data={[
+          { x: -2, y: 15 },
+          { x: -1, y: 10 },
+          { x: 0, y: 12 },
+          { x: 5, y: 8 },
+          { x: 6, y: 12 },
+          { x: 7, y: 14 },
+          { x: 8, y: 12 },
+          { x: 9, y: 13.5 },
+          { x: 10, y: 18 },
+        ]}
+        padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+        xDomain={{ min: -2, max: 10 }}
+        yDomain={{ min: -4, max: 20 }}
+      >
+        <VerticalAxis tickCount={10} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+        <HorizontalAxis tickCount={3} />
+        <Area theme={{ gradient: { from: { color: '#44bd32' }, to: { color: '#44bd32', opacity: 0.2 } } }} />
+        <Line theme={{ stroke: { color: '#44bd32', width: 5 } }} />
+      </Chart>
+      <Chart
+        style={{ height: 200, width: '100%' }}
+        data={[
+          { x: -2, y: 15 },
+          { x: -1, y: 10 },
+          { x: 0, y: 12 },
+          { x: 1, y: 7 },
+          { x: 2, y: 6 },
+          { x: 3, y: 3 },
+          { x: 4, y: 5 },
+          { x: 5, y: 8 },
+          { x: 6, y: 12 },
+          { x: 7, y: 14 },
+          { x: 8, y: 12 },
+          { x: 9, y: 13.5 },
+          { x: 10, y: 18 },
+        ]}
+        padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+        xDomain={{ min: -2, max: 10 }}
+        yDomain={{ min: -4, max: 20 }}
+      >
+        <VerticalAxis tickCount={10} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+        <HorizontalAxis tickCount={3} />
+        <Area theme={{ gradient: { from: { color: '#44bd32' }, to: { color: '#44bd32', opacity: 0.2 } } }} />
+        <Line
+          theme={{ stroke: { color: '#44bd32', width: 5 }, scatter: { default: { width: 8, height: 8, rx: 4, color: '#44ad32' }, selected: { color: 'red' } } }}
+        />
+      </Chart>
+      <Chart
+        style={{ height: 200, width: '100%' }}
+        data={[
+          { x: 5, y: 15 },
+          { x: 6, y: 6 },
+          { x: 7, y: 15 },
+          { x: 8, y: 3 },
+        ]}
+        padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+        xDomain={{ min: 5, max: 8 }}
+      >
+        <VerticalAxis
+          tickValues={[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]}
+          theme={{
+            axis: { stroke: { color: '#aaa', width: 2 } },
+            ticks: { stroke: { color: '#aaa', width: 2 } },
+            labels: { formatter: (v) => v.toFixed(2) },
+          }}
+        />
+        <HorizontalAxis
+          tickCount={9}
+          theme={{
+            axis: { stroke: { color: '#aaa', width: 2 } },
+            ticks: { stroke: { color: '#aaa', width: 2 } },
+            labels: { label: { rotation: 50 }, formatter: (v) => v.toFixed(1) },
+          }}
+        />
+        <Line theme={{ stroke: { color: 'red', width: 2 } }} />
+        <Line smoothing="bezier" tension={0.15} theme={{ stroke: { color: 'blue', width: 2 } }} />
+        <Line smoothing="bezier" tension={0.3} theme={{ stroke: { color: 'green', width: 2 } }} />
+        <Line smoothing="cubic-spline" tension={0.3} theme={{ stroke: { color: 'orange', width: 2 } }} />
+      </Chart>
     </Container>
   )
 }
 
 export default App
 
-const Container = styled.View`
+const Container = styled.ScrollView.attrs(() => ({ contentContainerStyle: { paddingBottom: 64 } }))`
   width: 100%;
   flex: 1;
   padding: 24px;
